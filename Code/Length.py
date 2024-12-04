e53f3ad5054af38a1ce91ad53a7fed2ad9e8dd8c
AbdulMujeeb Ahmed <149419208+abdulmujeeb4ahmed@users.noreply.github.com>
2024-12-03 20:22:37 -0500

    Add files via upload

---
+++ Code/Length.py
@@ -0,0 +1,2 @@
+def test_long_lines():
+    print("This is a very long line that is deliberately written to exceed the 80-character limit for testing purposes.")
---
+++ Code/SyntaxError.py
@@ -0,0 +1,4 @@
+def test_function():
+    print("This line has a syntax error"
+
+
